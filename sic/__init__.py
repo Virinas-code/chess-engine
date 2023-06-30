0141ac7cd9ae48b9b1c5e9d9cb2e772dd3d2f7e1
Virinas-code <alexcode228@gmail.com>
2023-06-30 14:48:04 +0200

    feat(license): First GPL comment

--- sic/__init__.py
+++ sic/__init__.py
@@ -4,6 +4,25 @@ Main CLI
 
 Used to start engine
 """
+# SIC - SImple Chess engine
+# Copyright (C) 2023  Virinas-code
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# A copy of the GNU General Public License can be found in the file
+# LICENSE
+
 from importlib.metadata import PackageMetadata, metadata
 from typing import Final
 
