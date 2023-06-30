b07adfc5f7933779ec2d79347126564b4b0eaa87
Virinas-code <alexcode228@gmail.com>
2023-06-30 14:48:53 +0200

    feat(license): Comment in all files

--- sic/uci/__init__.py
+++ sic/uci/__init__.py
@@ -4,6 +4,25 @@ UCI Interface
 
 Main class
 """
+# SIC - SImple Chess engine
+# Copyright (C) 2023  Virinas-code
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# A copy of the GNU General Public License can be found in the file
+# LICENSE
+
 from .commands import Commands
 
 
@@ -25,7 +44,7 @@ class UCI:
 
         Prints ID and starts main loop.
         """
-        self.commands.uci_id()
+        self.commands.uci_id([])
         self.main_loop()
 
     def main_loop(self) -> None:
--- sic/uci/commands.py
+++ sic/uci/commands.py
@@ -4,6 +4,25 @@ UCI commands
 
 Handles inputs
 """
+# SIC - SImple Chess engine
+# Copyright (C) 2023  Virinas-code
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# A copy of the GNU General Public License can be found in the file
+# LICENSE
+
 from typing import Callable, Final
 
 SUPPORTED_KEYWORDS: Final[list[str]] = ["id"]
